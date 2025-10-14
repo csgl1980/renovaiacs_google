5537c17dcb4215e5ecdfc9e1020c4fb148fd9d40
[dyad] <git@dyad.sh>
2025-10-14 11:26:00 -0300

    [dyad] Corrigindo erro de importação de LoginPage em index.tsx - wrote 1 file(s)

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import LoginPage from './pages/LoginPage';
+import LoginPage from "./pages/LoginPage.tsx";
 import { SessionContextProvider } from './components/SessionContextProvider';
 
 const rootElement = document.getElementById('root');
